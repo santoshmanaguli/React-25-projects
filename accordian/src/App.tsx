05a27e0e85ae72234bf4a8299c9957e56d45cbcc
Santosh Managuli <santoshmanaguli98@gmail.com>
2024-11-27 16:03:31 +0530

    commit

---
+++ accordian/src/App.tsx
@@ -0,0 +1,15 @@
+import Accordian from "./components/Accordian"
+
+function App() {
+  return (
+    <>
+      Accordian App
+
+      <br />
+      <br />
+      <Accordian></Accordian>
+    </>
+  )
+}
+
+export default App
